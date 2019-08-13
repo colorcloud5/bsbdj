e0867c22467e6fd744a2ace1c8e68f23092bef8b
colorcloud5 <495909546@qq.com>
2019-08-13 22:25:23 +0800

    项目初始化完成！

---
+++ Learning/Learning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Learning
+//
+//  Created by 龙超 on 2019/8/13.
+//  Copyright © 2019 caiyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
